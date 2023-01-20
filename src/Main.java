c7161e7f98dca8b891f8e9e69df0a8267f317b16
KarolBilas <karolbilas@wp.pl>
2023-01-20 20:00:41 +0100

    Working version with victory and lose conditions

--- src/Main.java
+++ src/Main.java
@@ -24,34 +24,56 @@ public class Main {
 
         String letters = "";
         boolean hasWon = false;
+        boolean hasLost = false;
         int tries = 10;
-        while(!hasWon) {
-            System.out.println("Guess the movie name! You have " + tries + "chances remaining");
+
+        while (!hasWon) {
+            System.out.println("Guess the movie name! You have " + tries + " chances remaining");
+            int j = 0;
             for (i = 0; i < selectedMovieLength; i++) {
+
                 String letterCheck = "";
                 letterCheck += selectedMovie.charAt(i);
 
-                if(letters.contains(letterCheck)){
-                   System.out.print(selectedMovie.charAt(i));
-                }
-                else if (selectedMovie.charAt(i) != ' ') {
+                if (letters.contains(letterCheck)) {
+                    System.out.print(selectedMovie.charAt(i));
+                    j++;
+                    if (j == selectedMovieLength) {
+                        hasWon = true;
+
+                    }
+                } else if (selectedMovie.charAt(i) != ' ') {
                     System.out.print("_");
                 } else {
                     System.out.print(" ");
+                    j++;
                 }
 
             }
-            System.out.println("");
-           System.out.println("Choose a letter: ");
-            Scanner scanner = new Scanner(System.in);
-            char letter = scanner.nextLine().charAt(0);
-            letters += letter;
+            if (!hasWon) {
+                System.out.println("");
+                System.out.println("Choose a letter: ");
+                Scanner scanner = new Scanner(System.in);
+                char letter = scanner.nextLine().charAt(0);
+                letters += letter;
 
+                String letterCheck = "";
+                letterCheck += letter;
 
-
+                if (!selectedMovie.contains(letterCheck)) {
+                    tries--;
+                    if (tries == 0) {
+                        hasLost = true;
+                        break;
+                    }
+                }
+            }
         }
-
-
-
+        if (hasLost) {
+            System.out.printf("You have lost the game");
+        }
+        if (hasWon) {
+            System.out.printf("You have won!");
+        }
     }
 }
